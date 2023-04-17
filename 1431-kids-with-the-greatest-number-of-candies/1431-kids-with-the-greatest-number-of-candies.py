d3d515e7c45359cc4c5eaa76d8e639fa7ab0b3fe
Wahid Sadique Koly <57163453+wskoly@users.noreply.github.com>
2023-04-17 15:46:08 +0600

    Time: 43 ms (39.91%), Space: 14 MB (8.81%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,5 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        
+        return list(map(lambda x: x+extraCandies >= max_candies, candies))
