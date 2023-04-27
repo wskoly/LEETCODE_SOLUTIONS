7d5b8c3b6e0bacb963df9bd64acb24a4cd816320
Wahid Sadique Koly <57163453+wskoly@users.noreply.github.com>
2023-04-27 12:46:24 +0600

    Time: 49 ms (8.42%), Space: 16.2 MB (34.09%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -1,6 +1,4 @@
 import math
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        if n == 0:
-            return 0
-        return math.floor(math.sqrt(n))
+        return int(math.sqrt(n))
